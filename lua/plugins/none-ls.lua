491c526c8b9bc042479e3d7f3488947f1b1c2989
David Ohlemacher <dohlemacher@infinidat.com>
2025-04-28 20:34:39 +0000

    Use 120 col for black line lengths
    DCO 1.1 Signed-off-by: David Ohlemacher <dohlemacher@infinidat.com>

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,14 +5,16 @@ return {
         local null_ls = require("null-ls")
 
         -- Soft links used when Mason cannot find a version that matches its required version
+        local line_length = "120"
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua, -- Lua:    Install using Mason
-                null_ls.builtins.formatting.black, -- Python: Install using Mason
-                null_ls.builtins.diagnostics.pylint, -- Python: Install using soft link to ~/.local/share/nvim/mason/bin.  Use :Telescope diagnosics.
-                -- null_ls.builtins.formatting.isort, -- Python: Install using soft link to ~/.local/share/nvim/mason/bin
-                -- null_ls.builtins.completion.spell,
-                -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+                null_ls.builtins.formatting.black.with({  -- Python: Install using Mason
+                    extra_args = { "--line-length", line_length },
+                }),
+                null_ls.builtins.diagnostics.pylint.with({  -- Python: Install using soft link to ~/.local/share/nvim/mason/bin.  Use :Telescope diagnosics.
+                    extra_args = { "--max-line-length=" .. line_length },
+                }),
             },
         })
 
